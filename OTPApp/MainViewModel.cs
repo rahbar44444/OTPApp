9d94500a8cda2d8deea0d235d4d25c690b598876
rahbar <rahbar@asztechnologies.com>
2023-06-04 18:05:36 +0530

    first commit

---
+++ OTPApp/MainViewModel.cs
@@ -0,0 +1,166 @@
+﻿using FluentEmail.Core;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+
+namespace OTPApp
+{
+    public class MainViewModel : BindableBase, IDataErrorInfo
+    {
+        private string _Email;
+        private string _OTP =null;
+        private bool _IsSendOTPEnable = false;
+        private bool _IsVerifyOTPEnable = false;
+        public bool IsTimeOut = false;
+        private static System.Timers.Timer Timer;
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; RaisePropertyChanged(); }
+        }
+
+        public string OTP
+        {
+            get { return _OTP; }
+            set { _OTP = value; RaisePropertyChanged(); }
+        }
+
+        public bool IsSendOTPEnable
+        {
+            get { return _IsSendOTPEnable; }
+            set { _IsSendOTPEnable = value; RaisePropertyChanged(); }
+        }
+
+        public bool IsVerifyOTPEnable
+        {
+            get { return _IsVerifyOTPEnable; }
+            set { _IsVerifyOTPEnable = value; RaisePropertyChanged(); }
+        }
+
+
+        public MainViewModel()
+        {
+            IsSendOTPEnable = false;
+            IsVerifyOTPEnable = false;
+        }
+
+        public void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            Timer = new System.Timers.Timer(60000);
+            // Hook up the Elapsed event for the timer. 
+            Timer.Elapsed += OnTimedEvent;
+            Timer.AutoReset = true;
+            Timer.Enabled = true;
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(OTP.ToString()))
+            {
+                IsTimeOut = true;
+                MessageBox.Show("Timeout after 1 min.\", \"STATUS_OTP_TIMEOUT");
+                UnSubscribeTimer();
+            }
+            else if (!(string.IsNullOrEmpty(OTP)) && OTP.ToArray().Count() ==6)
+            {
+                IsTimeOut = true;
+                UnSubscribeTimer();
+            }
+            else
+            {
+                UnSubscribeTimer();
+            }
+        }
+
+        public void UnSubscribeTimer()
+        {
+            Timer.Elapsed -= OnTimedEvent;
+            Timer.Close();
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = string.Empty;
+
+                switch (columnName)
+                {
+                    case nameof(Email):
+                        if (!string.IsNullOrEmpty(Email))
+                        {
+                            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                            Match match = regex.Match(Email);
+                            if (match.Success)
+                            {
+                                MailAddress address = new MailAddress(Email);
+                                string host = address.Host;
+                                if (host != "dso.org.sg")
+                                {
+                                    error = "Email doesn't belongs to correct domain.";
+                                    IsSendOTPEnable = false;
+                                }
+                                else
+                                {
+                                    IsSendOTPEnable = true;
+                                }
+                            }
+                            else
+                            {
+                                error = "Enter email in correct format.";
+                                IsSendOTPEnable = false;
+                            }
+                        }
+                        else if(string.IsNullOrEmpty(Email))
+                        {
+                            IsSendOTPEnable = false;
+                        }
+                        else
+                        {
+                            IsSendOTPEnable = true;
+                        }
+                        break;
+
+                    case nameof(OTP):
+                        if (!String.IsNullOrEmpty(OTP))
+                        {
+                            var otpValue = Int32.Parse(OTP);
+                            if (otpValue == 000000)
+                            {
+                                error = "000000 is not a valid OTP. Please enter valid OTP.";
+                                IsVerifyOTPEnable = false;
+                            }
+                            else if (OTP.ToArray().Count() <= 5)
+                            {
+                                error = "Please enter valid OTP.";
+                                IsVerifyOTPEnable = false;
+                            }
+                            else
+                            {
+                                IsVerifyOTPEnable = true;
+                            }
+                        }
+                        else
+                        {
+                            IsVerifyOTPEnable = false;
+                        }
+                        break;
+                }
+
+                return error;
+            }
+        }
+
+        public string Error => string.Empty;
+    }
+}
---
+++ OTPApp/MainWindow.xaml.cs
@@ -0,0 +1,179 @@
+﻿using FluentEmail.Core;
+using FluentEmail.Razor;
+using FluentEmail.Smtp;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OTPApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string CurrentOTP = string.Empty;
+        private int _Count=0;
+
+        public int Count
+        {
+            get { return _Count; }
+            set 
+            { 
+                _Count = value; 
+                if(Count>=10)
+                {
+                    MessageBox.Show("OTP is wrong after 10 tries.", "STATUS_OTP_FAIL");
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+
+        private async void SendOTP_Click(object sender, RoutedEventArgs e)
+        {
+           await SendOTP();
+        }
+
+        private void VerifyOTP_Click(object sender, RoutedEventArgs e)
+        {
+            if(!String.IsNullOrEmpty(CurrentOTP))
+            {
+                var mainVM = this.DataContext as MainViewModel;
+                if(mainVM !=null)
+                {
+                    if (mainVM.IsTimeOut == true) 
+                    {
+                        MessageBox.Show("Timeout after 1 min.", "STATUS_OTP_TIMEOUT");
+                        mainVM.UnSubscribeTimer();
+                        mainVM.OTP = string.Empty;
+                    }
+                    else if (mainVM.OTP.Equals(CurrentOTP))
+                    {
+                        MessageBox.Show("OTP is valid and checked.", "STATUS_OTP_OK");
+                        mainVM.UnSubscribeTimer();
+                        mainVM.OTP = string.Empty;
+                    }
+                    else
+                    {
+                        Count++;
+                    }
+                }
+            }
+        }
+
+        private async Task SendOTP()
+        {
+            var mainVM = this.DataContext as MainViewModel;
+            if (mainVM != null)
+            {
+                mainVM.IsTimeOut = false;
+                mainVM.OTP = string.Empty;
+                string[] allowedCharacters = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+
+                CurrentOTP = GenerateRandomOTP(6, allowedCharacters);
+
+                var senderConfig = new SmtpSender(() => new SmtpClient("localhost")
+                {
+                    EnableSsl = false,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Port = 25
+                    //DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory,
+                    //PickupDirectoryLocation = @"C:\Demos"
+                });
+
+                StringBuilder template = new();
+                template.AppendLine("Hi there,");
+                template.AppendLine("<p>You OTP Code is @Model.OTP. The code is valid for \r\n1 minute.</p>");
+                template.AppendLine("- The DSO Team");
+
+                Email.DefaultSender = senderConfig;
+                Email.DefaultRenderer = new RazorRenderer();
+
+                var email = await Email
+                    .From("rahbar@test.com")
+                    .To(mainVM.Email, "DSO")
+                    .Subject("OTP")
+                    .UsingTemplate(template.ToString(), new { OTP = CurrentOTP })
+                    .SendAsync();//.ConfigureAwait(true).GetAwaiter().GetResult();
+                mainVM.SetTimer();
+            }
+        }
+
+        private string GenerateRandomOTP(int iOTPLength, string[] saAllowedCharacters)
+        {
+            string sOTP = String.Empty;
+
+            string sTempChars = String.Empty;
+
+            Random rand = new Random();
+
+            for (int i = 0; i < iOTPLength; i++)
+
+            {
+
+                int p = rand.Next(0, saAllowedCharacters.Length);
+
+                sTempChars = saAllowedCharacters[rand.Next(0, saAllowedCharacters.Length)];
+
+                sOTP += sTempChars;
+
+            }
+
+            return sOTP;
+
+        }
+
+        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("^[1-9][0-9]{0,9}$");
+            //e.Handled = regex.IsMatch(e.Text);
+            if (regex.IsMatch(e.Text) == true)
+                e.Handled = false;
+            else if(e.Text == "0")
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void TextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                string text = (string)e.DataObject.GetData(typeof(String));
+                Regex regex = new Regex("[^0-9]+");
+                if (regex.IsMatch(text) == true)
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void textBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key >= Key.D0 && e.Key <= Key.D9)
+              || (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+              || e.Key == Key.Back)
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+    }
+}
